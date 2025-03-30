3c9eea508773ccc4d4ebe15ba644f4d74c322c47
viswa159 <53033350+viswa159@users.noreply.github.com>
2025-03-30 23:31:33 +0530

    Create Task02.py

---
+++ Task02.py
@@ -0,0 +1,6 @@
+list = [i for i in range(1,11)]
+print(f'Origirnal List: {list}')
+list1 = list[0:5]
+print(f'Extracted first five elements: {list1}')
+list1.reverse()
+print(f'Reversed extracted elements: {list1}')
